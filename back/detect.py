ac3376a444fb86bb11cdc5f96ae517f7116cc53d
Park_SH <mbc3940@kookmin.ac.kr>
2024-01-14 21:07:47 +0900

    크롤링 이미지 테스트

--- back/detect.py
+++ back/detect.py
@@ -7,7 +7,8 @@ model = YOLO("yolov8s.pt")
 image_paths = [
     # "/Users/park_sh/Desktop/what-desk/back/desk1.jpeg",
     # "/Users/park_sh/Desktop/what-desk/back/desk2.jpeg",  
-    "/Users/park_sh/Desktop/what-desk/back/desk3.jpeg"
+    # "/Users/park_sh/Desktop/what-desk/back/desk3.jpeg"
+    "/Users/park_sh/Desktop/what-desk/back/image/[3%쿠폰]_제로데스크_에보_테이블_컴퓨터_책상_1000~2000size_2Colors.jpg"
     # 추가 가능
 ]
 
