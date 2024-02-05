02cb5ea2bd1254963bd0adb98efa039825b086bc
Park_SH <mbc3940@kookmin.ac.kr>
2024-02-05 19:42:38 +0900

    유저 조회

--- back/database/crud.py
+++ back/database/crud.py
@@ -10,11 +10,10 @@ def create_user(db: Session, user: schemas.UserBase):
     return db_item
 
 def check_user(db: Session, id:str, password=str):
-    # 주어진 사용자 ID에 해당하는 정보가 데이터베이스에 있는지 확인
+    # id 확인
     existing_user = db.query(models.User).filter(models.User.id == id).first()
     
     if existing_user and existing_user.password != password:
-        # 이미 존재하고 비밀번호도 일치하는 경우
         return "비밀번호가 틀렸습니다"
     elif existing_user and existing_user.password == password:
         return existing_user
