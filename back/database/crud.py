3f41853f6c31753d2c5fbaa74da02ac1fc23d5ad
joonyoung1 <joonyoung1023@gmail.com>
2024-02-06 15:29:31 +0900

    hide user info in request body. fix crud functions to simplify.

--- back/database/crud.py
+++ back/database/crud.py
@@ -3,19 +3,11 @@ from database import models, schemas
 
 
 def create_user(db: Session, user: schemas.UserBase):
-    db_item = models.User(**user.dict())
+    db_item = models.User(user.model_dump())
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
     return db_item
 
-def check_user(db: Session, id:str, password=str):
-    # id 확인
-    existing_user = db.query(models.User).filter(models.User.id == id).first()
-    
-    if existing_user and existing_user.password != password:
-        return "비밀번호가 틀렸습니다"
-    elif existing_user and existing_user.password == password:
-        return existing_user
-    else:
-        return "유저가 없음"
+def read_user_by_id(db: Session, id: str):
+    return db.query(models.User).filter(models.User.id == id).first()
