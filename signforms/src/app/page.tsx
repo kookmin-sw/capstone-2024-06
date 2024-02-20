547f2a39d9ced9e9213507cbcbbce6d7a62873ba
Cho Hanul <135517709+3809271@users.noreply.github.com>
2024-02-20 21:32:18 +0900

    Create page.tsx

---
+++ signforms/src/app/page.tsx
@@ -0,0 +1,17 @@
+//signforms/src/app/page.tsx
+
+import React from 'react';
+import Test from './component/test';
+
+
+const MyPage = () => {
+  return (
+    <div>
+      <h1>My Page</h1>
+      <p>This is my page</p>
+      <Test />
+    </div>
+  );
+};
+
+export default MyPage;
