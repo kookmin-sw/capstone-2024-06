de75210e54f4486ee33a66e5f7556b9c7f526db9
Park_SH <mbc3940@kookmin.ac.kr>
2024-02-26 16:52:53 +0900

    process_image 모듈화

--- back/main.py
+++ back/main.py
@@ -5,6 +5,7 @@ import os
 import uuid
 
 from detect import detect
+from process_image import process
 from sqlalchemy.orm import Session
 from database import crud, models, schemas
 from database.database import SessionLocal, engine
@@ -12,7 +13,6 @@ from database.database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 
 def get_db():
 	db = SessionLocal()
@@ -48,23 +48,7 @@ async def get_image(image_filename: str):
 # 이미지 업로드 및 처리 결과 반환 
 @app.post("/process_image/")
 async def process_image(file: UploadFile):
-    try:
-        # 파일명을 UUID로 생성하여 유니크한 이름으로 저장
-        filename = f"{str(uuid.uuid4())}.jpg"
-        file_path = os.path.join(upload_folder, filename)
-
-        # 업로드된 파일 저장
-        with open(file_path, "wb") as image:
-            image.write(file.file.read())
-        # detect 함수를 적용하여 결과 반환
-        result_image_path = detect(file_path, result_folder)
-
-        # 결과물 파일명 반환
-        result_filename = os.path.basename(result_image_path)
-        return {"result_filename": result_filename}
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    return process(file)
 
 # 유저정보
 @app.post("/user/sign_up", response_model=schemas.UserBase)
