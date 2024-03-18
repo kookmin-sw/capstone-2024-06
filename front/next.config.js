5253bb2910a81218bfdf2199379d9797fd61f575
pgw <pgw00222@kookmin.ac.kr>
2024-03-18 19:40:53 +0900

    [main] 이미지 오류 수정

--- front/next.config.js
+++ front/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     Localhost: "http://10.223.114.14:8080",
   },
   images: {
-    domains: ['t1.kakaocdn.net', '10.223.114.14'], 
+    domains: ['t1.kakaocdn.net', '10.223.114.14', 'k.kakaocdn.net'], 
     remotePatterns: [
       {
         protocol: "https",
