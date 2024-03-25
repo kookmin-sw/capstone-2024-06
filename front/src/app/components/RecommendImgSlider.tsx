be7199f81f7b49593a3619ba100995ab54e4989a
pgw <pgw00222@kookmin.ac.kr>
2024-03-25 18:36:17 +0900

    [Front/analysis] 분석하기 페이지 디자인수정, 사진 미리보기 Modal 구현

--- front/src/app/components/RecommendImgSlider.tsx
+++ front/src/app/components/RecommendImgSlider.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+import Modal from "./Modal";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, FreeMode } from "swiper/modules";
@@ -10,6 +11,16 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const RecommendImgSlider = ({ Images }: { Images: string[] }) => {
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+
+  const handleImageClick = (imageUrl: string) => {
+    setPreviewImage(imageUrl);
+  };
+
+  const closePreview = () => {
+    setPreviewImage(null);
+  };
+
   return (
     <main className="flex w-full justify-center ">
       <div className="swiper-container w-[1000px] h-fit">
@@ -24,20 +35,26 @@ const RecommendImgSlider = ({ Images }: { Images: string[] }) => {
           className="mySwiper"
         >
           {Images.map((src, index) => (
-            
             <SwiperSlide key={index}>
-              <Image
-              src={src}
-                // src={`${process.env.Localhost}${src}`}
-                alt={`Desk ${index + 1}`}
-                width={300}
-                height={300}
-              
-              />
+              <div
+                className="w-[300px] h-[300px] relative"
+                onClick={() =>
+                  handleImageClick(`${process.env.Localhost}${src}`)
+                }
+              >
+                <Image
+                  src={`${process.env.Localhost}${src}`}
+                  alt={`Desk ${index + 1}`}
+                  layout="fill"
+                  objectFit="cover"
+                  className="cursor-pointer transition-transform transform hover:scale-105"
+                />
+              </div>
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
+      {previewImage && <Modal imageUrl={previewImage} onClose={closePreview} />}
     </main>
   );
 };
