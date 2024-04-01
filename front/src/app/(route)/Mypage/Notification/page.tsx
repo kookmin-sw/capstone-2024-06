418a6f8e43ba8251a7c47dcb5b1778d3520747f3
Cho Hanul <135517709+3809271@users.noreply.github.com>
2024-04-01 20:20:38 +0900

    add MyNotification

--- front/src/app/(route)/Mypage/Notification/page.tsx
+++ front/src/app/(route)/Mypage/Notification/page.tsx
@@ -9,19 +9,6 @@ import MyPagePosts from "../../../components/MyPagePosts";
 const Notification = () => {
   const { data: session } = useSession();
   const router = useRouter();
-  const [notifications, setNotifications] = useState([
-    {
-      id: 1,
-      message: "새로운 알림이 도착했습니다.",
-      date: new Date(),
-    },
-    {
-      id: 2,
-      message: "누군가가 당신의 게시물에 댓글을 남겼습니다.",
-      date: new Date(),
-    },
-    // 필요에 따라 추가할 수 있습니다.
-  ]);
 
   return (
     <>
@@ -32,18 +19,6 @@ const Notification = () => {
             <MyPageProfile />
             <MyPagePosts />
           </div>
-        </div>
-        <div className="absolute left-[647px] top-[149px] container mx-auto p-4">
-          <ul>
-            {notifications.map((notification) => (
-              <li key={notification.id} className="border-b py-2 w-[450px]">
-                <p className="text-lg">{notification.message}</p>
-                <p className="max-w-[400px] text-sm text-gray-500">
-                  {notification.date.toLocaleString()}
-                </p>
-              </li>
-            ))}
-          </ul>
         </div>
       </main>
     </>
