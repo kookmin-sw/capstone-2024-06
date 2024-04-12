487609d156d716257fe527286b8e85e0a6ce12a0
Park_SH <mbc3940@kookmin.ac.kr>
2024-04-12 09:51:44 +0900

    벡터 분류 모델 학습, pca 수정 kookmin-sw/capstone-2024-06#21

--- back/pca.py
+++ back/pca.py
@@ -1,36 +1,34 @@
-from sklearn.decomposition import PCA
-import os
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
-def load_vectors(vector_folder):
-    user_vectors = []
-    for vector_filename in os.listdir(vector_folder):
-        vector_path = os.path.join(vector_folder, vector_filename)
-        if os.path.isfile(vector_path) and vector_filename.lower().endswith(".npy"):
-            user_vector = np.load(vector_path)
-            if len(user_vector) > 0:  # Check if the vector is not empty
-                user_vectors.append(user_vector)
-            else:
-                print(f"Ignored empty vector file: {vector_path}")
-    return user_vectors
+# CSV 파일에서 데이터 불러오기
+data = pd.read_csv('transformed_data_with_labels.csv')
 
+# 라벨 추출
+labels = data['label']
 
-# 이미지 벡터를 로드합니다.
-vector_folder = "./vectors/train"
-image_vectors = load_vectors(vector_folder)
+# 벡터 데이터 추출 (라벨 열 제외)
+vectors = data.drop('label', axis=1).values
 
-# 적절한 차원 수를 결정합니다.
-n_components = 2
+# 시각화를 위한 색상 설정
+colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k', 'orange']  # 라벨에 따른 색상 설정
 
-# PCA를 사용하여 이미지 벡터를 2차원으로 축소합니다.
-pca = PCA(n_components=n_components)
-image_vectors_pca = pca.fit_transform(image_vectors)
-
-# 시각화를 위해 플롯합니다.
+# 시각화
 plt.figure(figsize=(10, 6))
-plt.scatter(image_vectors_pca[:, 0], image_vectors_pca[:, 1])
-plt.title('PCA Projection of Image Vectors')
+pca = PCA(n_components=2)  # PCA 객체 생성
+transformed_vectors = pca.fit_transform(vectors)  # PCA를 적용하여 벡터 데이터 변환
+
+for i in range(len(transformed_vectors)):
+    transformed_vector = transformed_vectors[i]
+    label = labels[i]
+
+    # 시각화
+    plt.scatter(transformed_vector[0], transformed_vector[1], c=colors[label], label=f'Label {label}')
+
+plt.title('PCA Projection of Vectors with Different Labels')
 plt.xlabel('Principal Component 1')
 plt.ylabel('Principal Component 2')
+plt.legend()
 plt.show()
